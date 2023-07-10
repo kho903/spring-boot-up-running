bdced5eb1cf3142b21f510043535d4fd6be09225
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-11 00:38:33 +0900

    feat: add coffee list in RestApiDemoController

---
+++ rest-demo/src/main/java/com/jikim/restdemo/RestApiDemoController.java
@@ -0,0 +1,20 @@
+package com.jikim.restdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+			new Coffee("Cafe Cereza"),
+			new Coffee("Cafe Ganador"),
+			new Coffee("Cafe Lareno"),
+			new Coffee("Cafe Tres Pontas")
+		));
+	}
+}
