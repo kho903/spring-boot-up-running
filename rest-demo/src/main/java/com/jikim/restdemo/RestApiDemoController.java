9127d84462416dad74124c3ea0202d044632d378
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-11 00:41:04 +0900

    feat: getCoffees by @RequestMapping

--- rest-demo/src/main/java/com/jikim/restdemo/RestApiDemoController.java
+++ rest-demo/src/main/java/com/jikim/restdemo/RestApiDemoController.java
@@ -3,6 +3,8 @@ package com.jikim.restdemo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +18,10 @@ public class RestApiDemoController {
 			new Coffee("Cafe Lareno"),
 			new Coffee("Cafe Tres Pontas")
 		));
+	}
+
+	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
+	Iterable<Coffee> getCoffees() {
+		return coffees;
 	}
 }
