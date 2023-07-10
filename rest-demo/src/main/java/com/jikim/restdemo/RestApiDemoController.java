d45d3675e0ff7f69eee66241932ef873e7eb4a73
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-11 00:52:54 +0900

    feat: postCoffee in RestApiDemoController

--- rest-demo/src/main/java/com/jikim/restdemo/RestApiDemoController.java
+++ rest-demo/src/main/java/com/jikim/restdemo/RestApiDemoController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,5 +36,11 @@ public class RestApiDemoController {
 			}
 		}
 		return Optional.empty();
+	}
+
+	@PostMapping("/coffees")
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
 	}
 }
