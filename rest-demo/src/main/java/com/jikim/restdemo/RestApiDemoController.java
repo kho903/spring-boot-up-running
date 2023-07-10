44e77adf068c8524c947b5f9296241d0d69da4ea
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-11 00:49:14 +0900

    feat: add getCoffeeById() in RestApiDemoController

--- rest-demo/src/main/java/com/jikim/restdemo/RestApiDemoController.java
+++ rest-demo/src/main/java/com/jikim/restdemo/RestApiDemoController.java
@@ -2,8 +2,10 @@ package com.jikim.restdemo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +24,15 @@ public class RestApiDemoController {
 	@GetMapping("/coffees")
 	Iterable<Coffee> getCoffees() {
 		return coffees;
+	}
+
+	@GetMapping("/coffees/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee coffee : coffees) {
+			if (coffee.getId().equals(id)) {
+				return Optional.of(coffee);
+			}
+		}
+		return Optional.empty();
 	}
 }
