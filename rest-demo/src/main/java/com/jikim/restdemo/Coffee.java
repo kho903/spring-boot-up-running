7ee1e49d59861c09a347a4d4a19c9bd9b08a637c
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-11 15:30:16 +0900

    feat: Coffee to JPA Entity

--- rest-demo/src/main/java/com/jikim/restdemo/Coffee.java
+++ rest-demo/src/main/java/com/jikim/restdemo/Coffee.java
@@ -2,9 +2,17 @@ package com.jikim.restdemo;
 
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Coffee {
-	private final String id;
+	@Id
+	private String id;
 	private String name;
+
+	public Coffee() {
+	}
 
 	public Coffee(String id, String name) {
 		this.id = id;
@@ -17,6 +25,10 @@ public class Coffee {
 
 	public String getId() {
 		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getName() {
